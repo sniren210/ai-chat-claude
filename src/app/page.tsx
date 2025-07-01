602c3f1888b48144307d851a00e4295ce7522b49
sniren-ren <sniren2002@gmail.com>
2025-07-01 15:40:34 +0700

    fix: icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
-"use client";
-
 import ChatContainer from "@/components/ChatContainer";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Flixscode.id - Transform Ideas into UI/UX",
+  description: "Transform your ideas into stunning UI/UX with a single prompt",
+};
 
 export default function Home() {
   return (
